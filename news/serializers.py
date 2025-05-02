ac737be33833d0b9b01d3f37e51bbd1ec433e840
elshangurbanow <elsenqurbanov50@gmail.com>
2025-05-02 19:10:10 +0000

    commit

---
+++ news/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import NewsThree
+
+
+class NewsThreeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsThree
+        fields = ['id', 'title', 'description', 'image']
